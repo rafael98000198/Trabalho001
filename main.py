8330a95b190ee04894d23eb8be8074ac43e0685b
rafaFD <rafael@boafoda.com>
2025-02-10 16:13:58 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Start')
+pygame.get_init()
+window = pygame.display.set_mode(size=(800, 600))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close window
+            quit()  # End pygame
